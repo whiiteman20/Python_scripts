0da870b6b233e8898ac1e071b63398f81215c69f
whiiteman20 <keads98@gmail.com>
2023-01-04 09:08:07 -0600

    Reorganizing

--- tts.py
+++
@@ -1,9 +0,0 @@
-from gtts import gTTS
-import os
-mytext = open('/home/kendale/Desktop/Programming/TTS/readthis.txt','r').read().replace('\n',' ')
-lang = 'en'
-
-speak = gTTS(text=mytext, lang=lang, slow=False)
-
-speak.save("read_textfile.mp3")
-
